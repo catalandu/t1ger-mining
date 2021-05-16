0524a89910860994ac8075c25234fc15bfd481ea
catalandu <84291747+catalandu@users.noreply.github.com>
2021-05-17 00:36:19 +0200

    Add files via upload

---
+++ t1ger_mining/config.lua
@@ -0,0 +1,39 @@
+Config = {
+    Prices = {
+        -- ['item'] = {min, max} --
+        ['steel'] = {40, 60},
+        ['iron'] = {50, 70},
+        ['copper'] = {60, 80},
+        ['diamond'] = {100, 150},
+        ['emerald'] = {100, 150}
+    },
+    ChanceToGetItem = 20, -- if math.random(0, 100) <= ChanceToGetItem then give item
+    Items = {'steel','steel','steel','steel','iron', 'iron', 'iron', 'copper', 'copper', 'diamond', 'emerald'},
+    Sell = vector3(-97.12, -1013.8, 26.3),
+    Objects = {
+        ['pickaxe'] = 'prop_tool_pickaxe',
+    },
+    MiningPositions = {
+        {coords = vector3(2992.77, 2750.64, 42.78), heading = 209.29},
+        {coords = vector3(2983.03, 2750.9, 42.02), heading = 214.08},
+        {coords = vector3(2976.74, 2740.94, 43.63), heading = 246.21}
+    },
+}
+
+Strings = {
+    ['press_mine'] = 'Press ~INPUT_CONTEXT~ to mine.',
+    ['mining_info'] = 'Press ~INPUT_ATTACK~ to chop, ~INPUT_FRONTEND_RRIGHT~ to stop.',
+    ['you_sold'] = 'You sold %sx %s for %s',
+    ['e_sell'] = 'Press ~INPUT_CONTEXT~ to sell all your mined items.',
+    ['someone_close'] = 'There is a player too close to you!',
+    ['mining'] = 'Mine',
+    ['sell_mine'] = 'Sell mined items'
+}  --[[  
+██╗░░░██╗██████╗░██╗░░░░░███████╗░█████╗░██╗░░██╗░██████╗
+██║░░░██║██╔══██╗██║░░░░░██╔════╝██╔══██╗██║░██╔╝██╔════╝
+██║░░░██║██████╔╝██║░░░░░█████╗░░███████║█████═╝░╚█████╗░
+██║░░░██║██╔═══╝░██║░░░░░██╔══╝░░██╔══██║██╔═██╗░░╚═══██╗
+╚██████╔╝██║░░░░░███████╗███████╗██║░░██║██║░╚██╗██████╔╝
+░╚═════╝░╚═╝░░░░░╚══════╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+█████████████████████████████████████████████████████████
+discord.gg/6CRxjqZJFB ]]--
